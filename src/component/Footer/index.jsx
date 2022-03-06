e508956748cae762ae2dae40747666d9a2e6d399
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-03-06 18:58:29 +0530

    Fix

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,17 +1,27 @@
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 import { animateScroll as scroll } from "react-scroll";
 import logoLight from "/icons/favicon.svg";
 import logoDark from "/icons/logo-dark.svg";
 
 const Footer = (props) => {
 
+    const footer = useRef(null);
+    let timeline = gsap.timeline({delay: 0.5});
+
     const scrollTop = () => {
         scroll.scrollToTop();
     }
 
+    useEffect(() => {
+        timeline.from(footer.current, {duration: 1, ease: "power3.easeIn", y: 100, opacity: 0}, 1);
+        timeline.play();
+    }, []);
+
     return (
         <div>
             <div className="px-8 sm:px-16 w-full min:h-32 border-t border-black/10 dark:border-white/5 flex items-center justify-center bg-white dark:bg-black dark:text-white">
-                <div className="flex flex-col sm:flex-row py-8 justify-between w-full items-center gap-3 text-black dark:text-white">
+                <div ref={footer} className="flex flex-col sm:flex-row py-8 justify-between w-full items-center gap-3 text-black dark:text-white">
                     <img src={props.mode === "dark" ? logoLight : logoDark} width="32px" />
                     <span className="text-lg">Copyright © 2022 Miyuru Gunarathna.</span>
                     <a href="#" onClick={scrollTop} className="rounded-3xl border border-black dark:border-white px-3 py-1 uppercase hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black">Back to Top</a>
--- src/modules/Contact/index.jsx
+++ src/modules/Contact/index.jsx
@@ -1,23 +1,39 @@
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
+
+    const message = useRef(null);
+    const contact = useRef(null);
+    const social = useRef(null);
+
+    let timeline = gsap.timeline({delay: 0.5});
+
+    useEffect(() => {
+        timeline.from(message.current.children, {duration: 1, ease: "power3.easeIn", x: -100, opacity: 0, stagger: {amount: 0.5}}, 1);
+        timeline.from(contact.current.children, {duration: 1, ease: "power3.easeIn", x: 100, opacity: 0, stagger: {amount: 0.5}}, 1);
+        timeline.from(social.current.children, {duration: 1, ease: "power3.easeIn", x: 100, opacity: 0, stagger: {amount: 0.5}}, 1);
+        timeline.play();
+    }, []);
+
     return (
         <div id="contact">
             <div className="p-8 sm:p-16 pt-24 sm:pt-24 min-h-screen items-stretch flex flex-col sm:flex-row gap-8 sm:gap-4 dark:text-white">
                 <div className="w-7/12">
-                    <h1 className="text-7xl tracking-tight font-light leading-[4.5rem] uppercase">
-                        I<br />
-                        would<br />
-                        love<br />
-                        to<br />
-                        hear<br />
-                        from<br />
-                        you.
+                    <h1 ref={message} className="text-7xl flex flex-col tracking-tight font-light leading-[4.5rem] uppercase">
+                        <span>I</span>
+                        <span>would</span>
+                        <span>love</span>
+                        <span>to</span>
+                        <span>hear</span>
+                        <span>from</span>
+                        <span>you</span>
                     </h1>
                 </div>
                 <div className="flex flex-col self-start sm:self-center pb-24 sm:pb-0 gap-10">
-                    <div className="flex flex-col gap-3">
+                    <div ref={contact} className="flex flex-col gap-3">
                         <h4 className="text-2xl font-semibold">Say Hello!</h4>
                         <p className="text-xl leading-6">
                             Wadugama Road<br />
@@ -35,7 +51,7 @@ const Contact = () => {
                             </a>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-3">
+                    <div ref={social} className="flex flex-col gap-3">
                         <h4 className="text-xl">See me on</h4>
                         <div className="flex flex-row gap-8">
                             <a href="https://www.linkedin.com/in/miyurugunarathna/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="xl" className="transition-all hover:rotate-6 hover:text-orange-500" /></a>
