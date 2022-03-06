b6089c0b919d94e52f422afe689c277dedf6cead
miyurugunarathna <33316556+miyurugunarathna@users.noreply.github.com>
2022-03-06 22:23:50 +0530

    Change the UI

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -1,30 +1,19 @@
-import { useRef, useEffect } from "react";
-import gsap from "gsap";
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-scroll";
 
 const Navbar = (props) => {
-
-    const header = useRef(null);
-    let timeline = gsap.timeline({ duration: 1, ease: "Power3.easeOut" });
-
-    useEffect(() => {
-        timeline.from(header.current, {y: -60}, 1);
-        timeline.play();
-    }, []);
-
     return (
         <div className="relative flex justify-center dark:text-white z-10">
-            <div ref={header} className="fixed top-0 w-full flex justify-between gap-8 px-8 sm:px-16 items-center backdrop-blur-lg h-16 font-semibold text-md bg-slate-200/70 border-b border-slate-900/10 dark:bg-neutral-900/80">
+            <div className="fixed top-0 w-full flex justify-between gap-8 px-8 sm:px-16 items-center backdrop-blur-[5px] saturate-150 h-16 font-semibold text-md bg-white/50 border-b border-black/10 dark:border-white/10 dark:bg-black/50">
                 <div className="flex gap-10">
-                    <Link className="hover:text-orange-500 cursor-pointer" activeClass="text-orange-500" spy={true} to="home" smooth={true} duration={1000} >Home</Link>
-                    <Link className="hover:text-orange-500 cursor-pointer" activeClass="text-orange-500" spy={true} to="work" smooth={true} duration={1000} >Work</Link>
-                    <Link className="hover:text-orange-500 cursor-pointer" activeClass="text-orange-500" spy={true} to="contact" smooth={true} duration={1000} >Contact</Link>
+                    <Link className="hover:text-purple-900 cursor-pointer transition-all duration-300" activeClass="text-purple-900" spy={true} to="home" smooth={true} duration={1000} >Home</Link>
+                    <Link className="hover:text-purple-900 cursor-pointer transition-all duration-300" activeClass="text-purple-900" spy={true} to="work" smooth={true} duration={1000} >Work</Link>
+                    <Link className="hover:text-purple-900 cursor-pointer transition-all duration-300" activeClass="text-purple-900" spy={true} to="contact" smooth={true} duration={1000} >Contact</Link>
                 </div>
                 <div>
-                    <button onClick={props.toggle} className="fade font-semibold flex items-center gap-1 hover:text-orange-500">
-                        <FontAwesomeIcon icon={props.mode === "dark" ? faMoon : faSun} />
+                    <button onClick={props.toggle} className="fade font-semibold flex items-center gap-1 hover:text-purple-900 transition-all duration-300">
+                        <FontAwesomeIcon className="hover:animate-spin" icon={props.mode === "dark" ? faMoon : faSun} />
                         <span className="hidden sm:block">{props.mode === "dark" ? "Dark" : "Light"}</span>
                     </button>
                 </div>
--- src/modules/Contact/index.jsx
+++ src/modules/Contact/index.jsx
@@ -1,28 +1,12 @@
-import { useRef, useEffect } from "react";
-import { gsap } from "gsap";
 import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
-
-    const message = useRef(null);
-    const contact = useRef(null);
-    const social = useRef(null);
-
-    let timeline = gsap.timeline({delay: 0.5});
-
-    useEffect(() => {
-        timeline.from(message.current.children, {duration: 1, ease: "power3.easeIn", x: -100, opacity: 0, stagger: {amount: 0.5}}, 1);
-        timeline.from(contact.current.children, {duration: 1, ease: "power3.easeIn", x: 100, opacity: 0, stagger: {amount: 0.5}}, 1);
-        timeline.from(social.current.children, {duration: 1, ease: "power3.easeIn", x: 100, opacity: 0, stagger: {amount: 0.5}}, 1);
-        timeline.play();
-    }, []);
-
     return (
         <div id="contact">
             <div className="p-8 sm:p-16 pt-24 sm:pt-24 min-h-screen items-stretch flex flex-col sm:flex-row gap-8 sm:gap-4 dark:text-white">
                 <div className="w-7/12">
-                    <h1 ref={message} className="text-7xl flex flex-col tracking-tight font-light leading-[4.5rem] uppercase">
+                    <h1 className="text-7xl flex flex-col tracking-tight font-light leading-[4.5rem] uppercase">
                         <span>I</span>
                         <span>would</span>
                         <span>love</span>
@@ -33,7 +17,7 @@ const Contact = () => {
                     </h1>
                 </div>
                 <div className="flex flex-col self-start sm:self-center pb-24 sm:pb-0 gap-10">
-                    <div ref={contact} className="flex flex-col gap-3">
+                    <div className="flex flex-col gap-3">
                         <h4 className="text-2xl font-semibold">Say Hello!</h4>
                         <p className="text-xl leading-6">
                             Wadugama Road<br />
@@ -43,20 +27,20 @@ const Contact = () => {
                             Sri Lanka
                         </p>
                         <div className="flex flex-col gap-1">
-                            <a className="text-xl font-semibold" href="mailto:miyurupriyawadan@gmail.com">
+                            <a className="text-xl font-semibold transition-all duration-300 hover:text-purple-900" href="mailto:miyurupriyawadan@gmail.com">
                                 miyurupriyawadan@gmail.com
                             </a>
-                            <a className="text-xl font-semibold" href="tel:+94776424889">
+                            <a className="text-xl font-semibold transition-all duration-300 hover:text-purple-900" href="tel:+94776424889">
                                 +94 77 642 4889
                             </a>
                         </div>
                     </div>
-                    <div ref={social} className="flex flex-col gap-3">
+                    <div className="flex flex-col gap-3">
                         <h4 className="text-xl">See me on</h4>
                         <div className="flex flex-row gap-8">
-                            <a href="https://www.linkedin.com/in/miyurugunarathna/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="xl" className="transition-all hover:rotate-6 hover:text-orange-500" /></a>
-                            <a href="https://github.com/miyurugunarathna" target="_blank"><FontAwesomeIcon icon={faGithub} size="xl" className="transition-all hover:rotate-6 hover:text-orange-500" /></a>
-                            <a href="https://twitter.com/miyurugunarathn" target="_blank"><FontAwesomeIcon icon={faTwitter} size="xl" className="transition-all hover:rotate-6 hover:text-orange-500" /></a>
+                            <a href="https://www.linkedin.com/in/miyurugunarathna/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="xl" className="transition-all duration-300 hover:text-purple-900" /></a>
+                            <a href="https://github.com/miyurugunarathna" target="_blank"><FontAwesomeIcon icon={faGithub} size="xl" className="transition-all duration-300 hover:text-purple-900" /></a>
+                            <a href="https://twitter.com/miyurugunarathn" target="_blank"><FontAwesomeIcon icon={faTwitter} size="xl" className="transition-all duration-300 hover:text-purple-900" /></a>
                         </div>
                     </div>
                 </div>
